ea8eb5e24ffa9f8692072f7f4d7e5cd23007d95a
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-18 13:41:42 +0700

    done

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wancak_clone/screen/widget/image_post_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.menu),
         title: Text('Showing Fun Posts'),
+        centerTitle: true,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.cloud_upload),
@@ -13,9 +16,32 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: null,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ImagePostWidget(),
+            ImagePostWidget(),
+            ImagePostWidget(),
+            ImagePostWidget(),
+            ImagePostWidget(),
+            ImagePostWidget(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {},
+                color: Colors.black45,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'More more fun',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screen/widget/image_post_widget.dart
+++ lib/screen/widget/image_post_widget.dart
@@ -1,15 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ImagePostWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(8),
+      width: MediaQuery.of(context).size.width,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Image(
-            image: NetworkImage('https://via.placeholder.com/200x600'),
+            image: NetworkImage('https://via.placeholder.com/350x200'),
+            fit: BoxFit.contain,
+            width: double.infinity,
           ),
-          Text('Velit reprehenderit dolore nostrud ipsum do irure.'),
+          SizedBox(height: 8),
+          Text(
+            'Velit reprehenderit dolore nostrud ipsum do irure.',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 4),
           RichText(
             text: TextSpan(
               children: <TextSpan>[
@@ -17,44 +31,49 @@ class ImagePostWidget extends StatelessWidget {
                   text: 'arifikhsan',
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
+                TextSpan(text: ' '),
                 TextSpan(text: '9 jam yang lalu'),
               ],
             ),
           ),
+          SizedBox(height: 4),
           Text('Dari rumah'),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.sentiment_satisfied,
-                ),
-                label: Text('Fun'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.sentiment_satisfied,
+                    ),
+                    label: Text('Fun'),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.sentiment_dissatisfied,
+                    ),
+                    label: Text('Nuf'),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.comment,
+                    ),
+                    label: Text('Komentar'),
+                  ),
+                ],
               ),
-              FlatButton.icon(
+              IconButton(
+                icon: Icon(Icons.more_vert),
                 onPressed: () {},
-                icon: Icon(
-                  Icons.sentiment_dissatisfied,
-                ),
-                label: Text('Nuf'),
-              ),
-              FlatButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.comment,
-                ),
-                label: Text('Komentar'),
-              ),
-              FlatButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.share,
-                ),
-                label: Text('Bagikan'),
               ),
             ],
           ),
+          SizedBox(height: 4),
           Text('500 fun'),
         ],
       ),
